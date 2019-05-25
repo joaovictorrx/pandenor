09008cff5178f4d06e97b07f9eda0f13284d844b
joao-atacama <joao@agenciaatacama.com.br>
2019-05-25 16:39:07 -0300

    final

--- wp-content/themes/pandenor/template-part/sub-navbar.php
+++ wp-content/themes/pandenor/template-part/sub-navbar.php
@@ -1,16 +1,6 @@
-<div class="secondary-nav d-none d-lg-flex justify-content-between">
-	<div class="left-side flex-fill"></div>
-	<div class="middle"></div>
-    <div class="right-side flex-fill position-relative">
-        <div class="position-absolute secondary-navbar-subtitle">
-            <?php if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-                <?php the_field('subtitle') ?>
-            <?php endwhile; wp_reset_postdata(); endif; ?>
-        </div>
-    </div>
-</div>
-<nav class="navbar navbar-expand-lg d-none d-lg-flex secondary-navbar">
-	<div class="container">
+<nav class="navbar navbar-expand-lg d-none d-lg-flex secondary-navbar px-0">
+
+	<div class="container" style="z-index: 1">
 		<div class="collapse navbar-collapse justify-content-between">
 			<ul class="navbar-nav flex-fill justify-content-between mt-2 mt-lg-0 secondary-navbar-title">
 				<li class="nav-item">
@@ -26,6 +16,17 @@
 				<li class="nav-item">
 				</li>
 			</ul>
+		</div>
+	</div>
+	<div class="secondary-nav d-none d-lg-flex justify-content-between">
+		<div class="left-side flex-fill"></div>
+		<div class="middle"></div>
+		<div class="right-side flex-fill position-relative">
+			<div class="position-absolute secondary-navbar-subtitle">
+				<?php if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+					<?php the_field('subtitle') ?>
+				<?php endwhile; wp_reset_postdata(); endif; ?>
+			</div>
 		</div>
 	</div>
 </nav>
